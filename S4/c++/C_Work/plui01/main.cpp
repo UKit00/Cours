9b0cac48fbf44b75befa808ba3292f0636856c8c
Usa De Kit <israelmaou@gmail.com>
2023-04-28 01:38:49 +0000

    Mise a jour c++ plui01

---
+++ S4/c++/C_Work/plui01/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int inputInt (string msg) {
+    int val;
+    cout << msg;
+    cin >> val;
+    return val;
+}
+
+int main()
+{
+    int nbMystere;
+    cout << "Programme de test de fonction inputInt" << endl;
+    int
+    return 0;
+}
